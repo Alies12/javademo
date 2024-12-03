002b60293bbd594c6b2e6bb96993c754d80d7a4e
andrey <singaevskiy2001@mai.ru>
2024-12-03 16:51:42 +0300

    base

--- demo/src/main/java/hexlet/code/demo/TheDatabase.java
+++ demo/src/main/java/hexlet/code/demo/TheDatabase.java
@@ -1,5 +1,14 @@
 package hexlet.code.demo;
 import java.sql.*;
 
-public class TheDatabase {
+public class TheDataBase {
+    public  static void thedatabase(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=andrejsingaevskij&password=stalker12&ssl=true";
+        Connection conn = DriverManager.getConnection(url);
+        String sql = "SELECT * FROM users";
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        System.out.println(result);
+    }
 }
